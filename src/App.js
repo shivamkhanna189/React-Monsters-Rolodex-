95e0965f092408679559ce82a51402f0e3632922
ShivamKhanna <shivamkhanna1995@gmail.com>
2020-05-09 22:51:46 +0530

    Adding Files for github pages.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {CardList} from "./components/card-list/card-list.component"
+import {SearchBox} from "./components/search-box/search-box.component";
+class App extends Component{
+
+    constructor(){
+      super();
+      this.state={
+        monsters :[],
+        searchFiled :""
+      }
+    }
+
+    componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users').
+      then(response =>response.json()).
+      then(users => this.setState({monsters  :users}));
+    }
+
+     render(){
+       const {monsters, searchFiled} = this.state
+        const filteredMonsters = monsters.filter(
+          data=>data.name.toLowerCase().includes(searchFiled.toLowerCase())
+        );
+        return (
+          <div className="App">
+            <h1>Monsters Rolodex</h1>
+            <SearchBox  placeholder="Search Box "  handleChange = {e=>this.setState({searchFiled : e.target.value})}/>
+            <CardList monsters = {filteredMonsters}> </CardList>
+            
+          </div>
+        );
+      }
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import './card-list.style.css';
+import {Card} from '../card/card.component';
+
+
+export const CardList = (props)=>{
+        console.log(props);
+        return (
+                <div className="card-list">
+                     { props.monsters.map(data=><Card key={data.id} monster={data}></Card>)
+                     }
+                </div>
+                
+            )
+};
+
